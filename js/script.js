8b6a701eaea466553e10bcf672a4548fa472e572
Miftahurrofi Asid-Qi <miftahurasidqi@gmail.com>
2023-01-15 04:52:08 +0700

    frist

---
+++ js/script.js
@@ -0,0 +1,7 @@
+// Hamburher
+const hamburger = document.querySelector("#hamburger");
+const navbar = document.querySelector("#navbar");
+
+hamburger.addEventListener("click", function () {
+  navbar.classList.toggle("hidden");
+});
