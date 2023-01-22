5657544f52d9e152ee030005009777f29c49d539
Miftahurrofi Asid-Qi <miftahurasidqi@gmail.com>
2023-01-23 05:15:38 +0700

    addUser

--- js/script.js
+++ js/script.js
@@ -5,3 +5,11 @@ const navbar = document.querySelector("#navbar");
 hamburger.addEventListener("click", function () {
   navbar.classList.toggle("hidden");
 });
+
+// addSurat
+const button = document.querySelector("#addArsip");
+const list = document.querySelector("#addSuratList");
+
+button.addEventListener("click", function () {
+  list.classList.toggle("hidden");
+});
